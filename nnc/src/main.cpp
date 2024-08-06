281657a2d596e5c1de73dae0f5b52b6b9b394df3
Patrick Harned <pat@li-819025cc-34a5-11b2-a85c-ba699012569d.ibm.com>
2024-08-06 16:00:26 -0400

    update

--- nnc/src/main.cpp
+++ nnc/src/main.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <fstream>
 #include <iostream>
 #include <ostream>
@@ -11,23 +12,24 @@ using namespace std;
 #define CLASSES 10
 #define HIDDEN 10
 
-double w1[HIDDEN][FEAT];
+double w1[FEAT][HIDDEN];
 
-double (*x_train)[ROW] = new double[COL][ROW];
+double (*x_train)[FEAT] = new double[ROW][FEAT];
+double *y = new double[ROW];
 double w2[HIDDEN][CLASSES];
 double b1[HIDDEN];
 double b2[CLASSES];
-double (*z1)[ROW] = new double[HIDDEN][ROW];
-double (*z2)[ROW] = new double[CLASSES][ROW];
-double (*a1)[ROW] = new double[HIDDEN][ROW];
-double (*a2)[ROW] = new double[HIDDEN][ROW];
-double (*dz1)[ROW] = new double[HIDDEN][ROW];
-double (*dz2)[ROW] = new double[CLASSES][ROW];
+double (*z1)[HIDDEN] = new double[ROW][HIDDEN];
+double (*z2)[CLASSES] = new double[ROW][CLASSES];
+double (*a1)[HIDDEN] = new double[ROW][HIDDEN];
+double (*a2)[HIDDEN] = new double[ROW][CLASSES];
+double (*dz1)[HIDDEN] = new double[ROW][HIDDEN];
+double (*dz2)[CLASSES] = new double[ROW][CLASSES];
 
-double dw2[HIDDEN][CLASSES];
+double dw2[CLASSES][HIDDEN];
 double db2[CLASSES];
 double db1[HIDDEN];
-double dw1[HIDDEN][FEAT];
+double dw1[FEAT][HIDDEN];
 
 int main() {
   std::ifstream file("data.csv");
@@ -42,7 +44,11 @@ int main() {
     std::istringstream iss(line);
     double value;
     while (iss >> value) {
-      x_train[col][row] = static_cast<double>(value);
+      if (col == 0) {
+        y[row] = static_cast<double>(value);
+      } else {
+        x_train[row][col - 1] = static_cast<double>(value);
+      }
       col++;
     }
     col = 0;
@@ -59,11 +65,69 @@ int main() {
   std::mt19937 gen(rd());
   std::uniform_real_distribution<double> dis(0.0, 1.0);
 
-  for (int i = 0; i < HIDDEN; i++) {
-    for (int j = 0; j < FEAT; j++) {
+  for (int i = 0; i < FEAT; i++) {
+    for (int j = 0; j < HIDDEN; j++) {
       w1[i][j] = dis(gen);
     }
   }
 
+  for (int i = 0; i < HIDDEN; i++) {
+    for (int j = 0; j < CLASSES; j++) {
+      w2[i][j] = dis(gen);
+    }
+  }
+
+  for (int i = 0; i < HIDDEN; i++) {
+    b1[i] = dis(gen);
+  }
+  for (int i = 0; i < HIDDEN; i++) {
+    b2[i] = dis(gen);
+  }
+
+  double(*one_hot_y)[ROW] = new double[CLASSES][ROW];
+
+  for (int i = 0; i < ROW; i++) {
+    for (int j = 0; j < CLASSES; j++) {
+      one_hot_y[j][i] = 0;
+    }
+    one_hot_y[static_cast<int>(x_train[0][i])][i] = 1;
+  }
+
+  // should equal 1
+  cout << one_hot_y[5][0] << endl;
+
+  // training loop
+  //
+  //
+  // we first calculate Z1 as the dot product of W1 and x_train
+  //
+  for (int i = 0; i < 1; i++) {
+    // z1
+    for (int i = 0; i < FEAT; i++) {
+      for (int j = 0; j < ROW; j++) {
+        for (int k = 1; k < HIDDEN; k++) {
+          z1[j][i] = w1[i][k] * x_train[j][i] + b1[k];
+        }
+      }
+    }
+
+    // a1 is our output layer, we apply relu
+    for (int i = 0; i < ROW; i++) {
+      for (int j = 0; j < HIDDEN; j++) {
+        a1[i][j] = (z1[i][j] < 0) ? 0 : z1[i][j];
+      }
+    }
+
+    // pass through second layer
+    for (int i = 0; i < CLASSES; i++) {
+      for (int j = 0; j < ROW; j++) {
+        for (int k = 0; k < HIDDEN; k++) {
+          z2[i][j] = w2[i][k] * a1[k][j] + b2[k];
+        }
+      }
+    }
+  }
+
+  cout << a1[ROW - 1][HIDDEN - 1] << endl;
   return 0;
 }
