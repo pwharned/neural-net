3a19aec317ef1e1073c791ad059de4fbc3ebee99
Patrick Harned <pat@li-819025cc-34a5-11b2-a85c-ba699012569d.ibm.com>
2024-08-07 19:54:57 -0400

    update

--- nnc/src/main.cpp
+++ nnc/src/main.cpp
@@ -11,28 +11,39 @@ using namespace std;
 #define COL 785
 #define FEAT COL - 1
 #define CLASSES 10
-#define HIDDEN 10
+#define HIDDEN 12
 
-double (*x_train)[FEAT] = new double[ROW][FEAT];
+double (*x_train)[ROW] = new double[FEAT][ROW];
 double *y = new double[ROW];
 
-double w1[FEAT][HIDDEN];
-double w2[HIDDEN][CLASSES];
+double w1[HIDDEN][FEAT];
+double w2[CLASSES][HIDDEN];
 
 double b1[HIDDEN];
 double b2[CLASSES];
-double (*z1)[HIDDEN] = new double[ROW][HIDDEN];
-double (*z2)[CLASSES] = new double[ROW][CLASSES];
-double (*a1)[HIDDEN] = new double[ROW][HIDDEN];
-double (*a2)[HIDDEN] = new double[ROW][CLASSES];
-double (*dz1)[HIDDEN] = new double[ROW][HIDDEN];
-double (*dz2)[CLASSES] = new double[ROW][CLASSES];
+double (*z1)[ROW] = new double[HIDDEN][ROW];
+double (*a1)[ROW] = new double[HIDDEN][ROW];
+
+double (*z2)[ROW] = new double[CLASSES][ROW];
+double (*a2)[ROW] = new double[CLASSES][ROW];
+
+double (*dz1)[ROW] = new double[HIDDEN][ROW];
+double (*dz2)[ROW] = new double[CLASSES][ROW];
 
 double dw2[CLASSES][HIDDEN];
+
 double db2[CLASSES];
 double db1[HIDDEN];
-double dw1[FEAT][HIDDEN];
+
+double (*dw1)[FEAT] = new double[HIDDEN][FEAT];
 double (*one_hot_y)[ROW] = new double[CLASSES][ROW];
+
+double exp_sum = 0.0;
+int max_idx = 0;
+int ground_truth = 0;  // the index value of the ground truth;
+int n_correct = 0;     // the number of correct predictions
+double exp_max = 0;
+double alpha = 0.4;
 
 int main() {
   std::ifstream file("data.csv");
@@ -50,7 +61,7 @@ int main() {
       if (col == 0) {
         y[row] = static_cast<double>(value);
       } else {
-        x_train[row][col - 1] = static_cast<double>(value);
+        x_train[col - 1][row] = static_cast<double>(value) / 255;
       }
       col++;
     }
@@ -60,7 +71,6 @@ int main() {
   file.close();
 
   cout << "Read " << row << " rows" << endl;
-  cout << x_train[0][0] << endl;
 
   // populate the weights;
   //
@@ -70,12 +80,12 @@ int main() {
 
   for (int i = 0; i < FEAT; i++) {
     for (int j = 0; j < HIDDEN; j++) {
-      w1[i][j] = dis(gen);
+      w1[j][i] = dis(gen);
     }
   }
 
-  for (int i = 0; i < HIDDEN; i++) {
-    for (int j = 0; j < CLASSES; j++) {
+  for (int i = 0; i < CLASSES; i++) {
+    for (int j = 0; j < HIDDEN; j++) {
       w2[i][j] = dis(gen);
     }
   }
@@ -83,7 +93,7 @@ int main() {
   for (int i = 0; i < HIDDEN; i++) {
     b1[i] = dis(gen);
   }
-  for (int i = 0; i < HIDDEN; i++) {
+  for (int i = 0; i < CLASSES; i++) {
     b2[i] = dis(gen);
   }
 
@@ -97,41 +107,33 @@ int main() {
   // should equal 1
   cout << one_hot_y[5][0] << endl;
 
-  // training loop
-  //
-  //
-  // we first calculate Z1 as the dot product of W1 and x_train
-  //
-  //
   for (int it = 0; it < 10; it++) {
-    for (int i = 0; i < 1; i++) {
-      // z1
-      for (int i = 0; i < FEAT; i++) {
-        for (int j = 0; j < ROW; j++) {
-          for (int k = 1; k < HIDDEN; k++) {
-            z1[j][i] = (w1[i][k] * x_train[j][i]) + b1[k];
-            a1[i][j] = (z1[i][j] < 0) ? 0 : z1[i][j];
-          }
-        }
-      }
-
-      // pass through second layer ( dot product of w2 with a1 + the second bias
-      // term)
-      for (int i = 0; i < CLASSES; i++) {
-        for (int j = 0; j < ROW; j++) {
-          for (int k = 0; k < HIDDEN; k++) {
-            z2[j][i] = (w2[k][i] * a1[k][j]) + b2[i];
-          }
+    // training loop
+    //
+    //
+    // we first calculate Z1 as the dot product of W1 and x_train
+    //
+    //
+    n_correct = 0;
+    // z1
+    for (int i = 0; i < HIDDEN; i++) {
+      for (int j = 0; j < ROW; j++) {
+        for (int k = 1; k < FEAT; k++) {
+          z1[i][j] = (w1[i][k] * x_train[k][j]) + b1[i];
+          a1[i][j] = (z1[i][j] < 0) ? 0 : z1[i][j];
         }
       }
     }
 
-    double exp_sum = 0.0;
-    int max_idx = 0;
-    int ground_truth = 0;  // the index value of the ground truth;
-    int n_correct = 0;     // the number of correct predictions
-    double exp_max = 0;
-    double alpha = 0.4;
+    // pass through second layer ( dot product of w2 with a1 + the second bias
+    // term)
+    for (int i = 0; i < CLASSES; i++) {
+      for (int j = 0; j < ROW; j++) {
+        for (int k = 0; k < HIDDEN; k++) {
+          z2[i][j] = (w2[i][k] * a1[k][j]) + b2[i];
+        }
+      }
+    }
 
     /// In this loop we calculate the softmax of the z2 (a2), followed by the
     /// derivative dz2  as the absolute difference between a2 and y and db2
@@ -139,17 +141,19 @@ int main() {
     /// accuracy by checking the prediction (Z2) against the y value.
     for (int i = 0; i < ROW; i++) {
       for (int j = 0; j < CLASSES; j++) {
-        a2[i][j] = exp(z2[i][j]);
+        a2[j][i] = exp(z2[j][i]);
         exp_sum += a2[j][i];
-        dz2[i][j] = a2[i][j] - one_hot_y[j][i];
-        db2[j] += dz2[i][j];
-        b2[j] -= (db1[j] * alpha);
       }
+      exp_max = 0.0;
+      max_idx = 0;
       for (int j = 0; j < CLASSES; j++) {
-        a2[i][j] /= exp_sum;  // calculate the softmax
+        a2[j][i] /= exp_sum;
+        // calculate the softmax
+        dz2[j][i] = a2[j][i] - one_hot_y[j][i];
+        db2[j] += dz2[j][i];
 
-        if (a2[i][j] > exp_max) {
-          exp_max = a2[i][j];
+        if (a2[j][i] > exp_max) {
+          exp_max = a2[j][i];
           max_idx = j;
 
         }  // determine whether th
@@ -161,13 +165,16 @@ int main() {
       exp_sum = 0.0;
     }
 
+    for (int i = 0; i < CLASSES; i++) {
+      b2[i] -= (db1[i] * alpha);
+    }
+
     // calculate dW2 as the derivative of loss wrt W2 - this is the dot product
     // of dz2 (60000X10) with the transpose of  A1 (10x60000)
-
-    for (int i = 0; i < HIDDEN; i++) {
-      for (int j = 0; j < CLASSES; j++) {
+    for (int i = 0; i < CLASSES; i++) {
+      for (int j = 0; j < HIDDEN; j++) {
         for (int k = 0; k < ROW; k++) {
-          dw2[i][j] += dz2[k][j] * a1[k][i];
+          dw2[i][j] += dz2[i][k] * a1[j][k];
           w2[i][j] -= (dw1[i][j] * alpha);  // update w2
         }
       }
@@ -175,17 +182,15 @@ int main() {
 
     // dZ1 : transpose of  W2  * dZ2 *  (relu derivative of Z1)
     //
-
-    for (int i = 0; i < CLASSES; i++) {
+    for (int i = 0; i < HIDDEN; i++) {
       for (int j = 0; j < ROW; j++) {
-        for (int k = 0; k < HIDDEN; k++) {
-          dz1[j][i] += (w2[k][i] * dz2[j][i] * (z1[j][i] > 0 ? 1 : 0));
+        for (int k = 0; k < CLASSES; k++) {
+          dz1[i][j] += (w2[k][i] * dz2[k][j] * (z1[i][j] > 0 ? 1 : 0));
         }
-        db1[i] += dz1[j][i];
-        b1[i] -= (b1[i] * alpha);
+        db1[i] += dz1[i][j];
+        b1[i] -= (db1[i] * alpha);
       }
     }
-
     // dw1 the derivative of loss wrt w1(FEATxHIDDEN) = (10 x 59999) x (59999 X
     // 784)
     //
@@ -193,8 +198,8 @@ int main() {
     for (int i = 0; i < HIDDEN; i++) {
       for (int j = 0; j < FEAT; j++) {
         for (int k = 0; k < ROW; k++) {
-          dw1[j][i] += dz1[i][k] * x_train[k][j];
-          w1[j][i] -= (dw1[j][i] * alpha);
+          dw1[i][j] += dz1[i][k] * x_train[j][k];
+          w1[i][j] -= (dw1[i][j] * alpha);
         }
       }
     }
